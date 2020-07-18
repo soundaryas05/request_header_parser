70c0b8863ec708e2ce1ffbd4a940d4d8b01c8c59
Glitch (hilarious-materialistic-headphones) <none>
2020-07-18 14:12:36 +0000

    🏁🌥 Checkpoint ./server.js:16377575/568

--- server.js
+++ server.js
@@ -20,8 +20,12 @@ app.get("/", function (req, res) {
 
 
 // your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+app.get("/api/whoami", function (req, res) {
+  var software = req.headers['user-agent'];
+  var language = req.headers['accept-language'];
+  var ip_address = req.connection.remoteAddress;
+  res.json({ipaddress: ip_address,language: language,
+software: software});
 });
 
 
